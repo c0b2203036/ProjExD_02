a9ae5eea356aaa93afc3d16e3633afa2f96ab395
takuoba <c0b2203036@edu.teu.ac.jp>
2023-06-20 14:30:23 +0900

    練習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,14 +12,15 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    bb_img = pg.Surface((20,20))
+    bb_img = pg.Surface((20,20)) #練習1
     bb_img.set_colorkey((0, 0, 0))  # 黒い部分を透明にする
     pg.draw.circle(bb_img,(255,0,0),(10,10),10)
     x = random.randint(0,WIDTH)
     y = random.randint(0,HEIGHT)
     bb_rct = bb_img.get_rect() #爆弾Surface（bd_img）から爆弾Rect（bd_rct）を抽出する
     bb_rct.center = x, y # 爆弾Rectの中心座標を乱数で指定する
-    
+    vx, vy = +5, +5 #練習２
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -29,6 +30,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bb_rct.move_ip(vx, vy)  # 練習２
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
